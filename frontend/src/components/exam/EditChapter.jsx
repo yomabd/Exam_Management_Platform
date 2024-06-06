1579ebaa13a0973d2548e81b4eff080e4fe47674
yomabd <idreesyomi@gmail.com>
2024-06-06 17:37:49 +0100

    exam Edit operation in progress

---
+++ frontend/src/components/exam/EditChapter.jsx
@@ -0,0 +1,256 @@
+import React, { useState,useEffect } from 'react';
+import { FormGroup, FormLabel, Input, Select, Button } from './FormComponents';
+import axios from 'axios';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+// import { useParams } from 'react-router-dom';
+
+const EditChapter = ({qid}) => {
+  const [chapterName, setChapterName] = useState('');
+  const [time, setTime] = useState('');
+  const [instruction, setInstruction] = useState({
+    heading: '',
+    paragraphs: ['', '', ''],
+  });
+  const [questions, setQuestions] = useState([
+    { question: '', options: ['', '', '', '', ''], correctAnswer: '' },
+  ]);
+  const baseUrl = "http://localhost:3005/api/questionBanks";
+  // const params = useParams();
+
+  useEffect(() => {
+    console.log("QID is "+qid)
+    axios.get(`${baseUrl}/${qid}/chapters`)
+    .then((response)=>{
+      console.log(response.data)
+    })
+    .catch((error)=>{
+      console.error('error occurs while fetching...', error)
+    })
+
+  
+  }, [])
+
+  const validateForm = () => {
+    if (!chapterName || !time ||!instruction.paragraphs[0]) {
+      toast.error("Please fill all required fields.");
+      return false;
+    }
+    if(question.question) {
+      if (!question.option[0] || !option[1]){
+      toast.error("You can't set incomplete questions");
+      return false;
+    }
+  }
+    return true;
+  };
+
+  const removeQuestion = (index) => {
+    const updatedQuestions = [...questions];
+    updatedQuestions.splice(index, 1);
+    setQuestions(updatedQuestions);
+  };
+  
+  
+  const handleInstructionChange = (index, value) => {
+    const newParagraphs = [...instruction.paragraphs];
+    newParagraphs[index] = value;
+    setInstruction({ ...instruction, paragraphs: newParagraphs });
+  };
+
+  const handleQuestionChange = (index, value) => {
+    const newQuestions = [...questions];
+    newQuestions[index].question = value;
+    setQuestions(newQuestions);
+  };
+
+  const handleOptionChange = (qIndex, oIndex, value) => {
+    const newQuestions = [...questions];
+    newQuestions[qIndex].options[oIndex] = value;
+    setQuestions(newQuestions);
+  };
+
+  const handleCorrectAnswerChange = (qIndex, value) => {
+    const newQuestions = [...questions];
+    newQuestions[qIndex].correctAnswer = value;
+    setQuestions(newQuestions);
+  };
+
+  const addQuestion = () => {
+    setQuestions([
+      ...questions,
+      { question: '', options: ['', '', '', '', ''], correctAnswer: '' },
+    ]);
+  };
+
+  const handleAddChapter = async (e) => {
+    e.preventDefault();
+    if (!validateForm()) return;
+    const chapterData = {
+      name: chapterName,
+      time,
+      instruction,
+      questions,
+    };
+    console.log(chapterData);
+    try {
+      const response = await axios.post(`${baseUrl}/${qid}/chapters`,
+        chapterData); 
+      console.log('Chapter added:', response.data);
+      // Clear form after submission
+      setChapterName('');
+      setTime('');
+      setInstruction({ heading: '', paragraphs: ['', '', ''] });
+      setQuestions([{ question: '', options: ['', '', '', '', ''], correctAnswer: '' }]);
+    } catch (error) {
+      console.error('Error adding chapter:', error);
+    }
+  };
+
+  return (
+    <div className="max-w-4xl mx-auto p-6 bg-white shadow-md rounded-md">
+      <h2 className="text-2xl font-bold mb-4">Create Chapter</h2>
+      <form onSubmit={handleAddChapter}>
+        <div className="mb-6">
+          <h3 className="text-xl font-semibold mb-2">Chapter Details</h3>
+          <FormGroup>
+            <FormLabel htmlFor="chapterName" required>
+              Chapter Name:
+            </FormLabel>
+            <Input
+              type="text"
+              id="chapterName"
+              value={chapterName}
+              onChange={(e) => setChapterName(e.target.value)}
+              required
+            />
+          </FormGroup>
+          <FormGroup>
+            <FormLabel htmlFor="time" required>Time (in minutes):</FormLabel>
+            <Input
+              type="number"
+              id="time"
+              value={time}
+              min={0}
+              onChange={(e) => setTime(e.target.value)}
+              required
+            />
+          </FormGroup>
+        </div>
+
+        <div className="mb-6">
+          <h3 className="text-xl font-semibold mb-2">Instruction</h3>
+          <FormGroup>
+            <FormLabel htmlFor="instructionHeading">Instruction Heading:</FormLabel>
+            <Input
+              type="text"
+              id="instructionHeading"
+              value={instruction.heading}
+              onChange={(e) => setInstruction({ ...instruction, heading: e.target.value })}
+            />
+          </FormGroup>
+          <FormGroup>
+            <FormLabel htmlFor="instructionParagraph1" required>
+              Instruction Paragraph 1:
+            </FormLabel>
+            <Input
+              type="text"
+              id="instructionParagraph1"
+              value={instruction.paragraphs[0]}
+              onChange={(e) => handleInstructionChange(0, e.target.value)}
+              required
+            />
+          </FormGroup>
+          <FormGroup>
+            <FormLabel htmlFor="instructionParagraph2">Instruction Paragraph 2:</FormLabel>
+            <Input
+              type="text"
+              id="instructionParagraph2"
+              value={instruction.paragraphs[1]}
+              onChange={(e) => handleInstructionChange(1, e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup>
+            <FormLabel htmlFor="instructionParagraph3">Instruction Paragraph 3:</FormLabel>
+            <Input
+              type="text"
+              id="instructionParagraph3"
+              value={instruction.paragraphs[2]}
+              onChange={(e) => handleInstructionChange(2, e.target.value)}
+            />
+          </FormGroup>
+        </div>
+
+        {questions.map((question, qIndex) => (
+  <div key={qIndex} className="mb-6">
+    <h3 className="text-xl font-semibold mb-2">Question {qIndex + 1}</h3>
+    <Button type="button" onClick={() => removeQuestion(qIndex)}>Remove Question</Button>
+            <FormGroup>
+              <FormLabel htmlFor={`question${qIndex}`} required ={question.question.trim()!==""}>
+                Question:
+              </FormLabel>
+              <Input
+                type="text"
+                id={`question${qIndex}`}
+                value={question.question}
+                onChange={(e) => handleQuestionChange(qIndex, e.target.value)}
+                required
+              />
+            </FormGroup>
+            {question.options.map((option, oIndex) => (
+              <FormGroup key={oIndex}>
+                <FormLabel htmlFor={`option${qIndex}-${oIndex}`} required={oIndex === 0 && question.question.trim()!=='' || oIndex ===1
+              && question.question.trim()!==''}>
+                  Option {oIndex + 1}:
+                </FormLabel>
+                <Input
+                  type="text"
+                  id={`option${qIndex}-${oIndex}`}
+                  value={option}
+                  onChange={(e) => handleOptionChange(qIndex, oIndex, e.target.value)}
+                  required={(oIndex === 0  || oIndex ===1)
+                  && question.question.trim()!==''}
+                />
+              </FormGroup>
+            ))}
+            <FormGroup>
+              <FormLabel htmlFor={`correctAnswer${qIndex}`} required={question.question.trim()!==''}>
+                Correct Answer:
+              </FormLabel>
+              <Select
+                id={`correctAnswer${qIndex}`}
+                value={question.correctAnswer}
+                onChange={(e) => handleCorrectAnswerChange(qIndex, e.target.value)}
+                required ={question.options[0] && question.options[1]
+                  && question.question.trim()!==''}
+              >
+                <option value="">Select Correct Answer</option>
+                {question.options.map((option, oIndex) => (
+                  <option key={oIndex} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </Select>
+            </FormGroup>
+          </div>
+        ))}
+
+        <div className="flex justify-between gap-4 mt-4">
+          <Button type="button" onClick={addQuestion}>
+            Add Question
+          </Button>
+          <Button type="submit" onClick={handleAddChapter}>Save Chapter</Button>
+          <Button type="button" onClick={() => (window.location.href = '/dashboard')}>
+            Go to Dashboard
+          </Button>
+          <Button type="button" onClick={() => (window.location.href = '/exams')}>
+            Exams
+          </Button>
+        </div>
+      </form>
+      <ToastContainer/>
+    </div>
+  );
+};
+
+export default EditChapter;
---
+++ frontend/src/components/exam/EditExam.jsx
@@ -0,0 +1,274 @@
+import React, { useState } from "react";
+import { FormGroup, FormLabel, Input, Select, Button } from "../dashboard/FormComponents";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useEffect } from "react";
+import { fetchQuestionBanks } from "../dashboard/fetchQuestionBankFunction";
+import Spinner from "../Spinner";
+
+
+
+
+
+const EditExam = ({qid}) => {
+  const [examname, setExamName] = useState('');
+  const [examlevel, setExamLevel] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [time, setTime] = useState('');
+//   const []
+  const [questionBank, setQuestionBank] = useState([])
+  const [chaptersMode, setChaptersMode] = useState('');
+  const navigate = useNavigate();
+  const [showEditChapters, setShowEditChapters] = useState(false);
+  const [chapters, setChapters] =  useState([]);
+  const [generalInstruction, setGeneralInstruction] = useState({
+    heading: '',
+    paragraphs: ['', '', '']
+  });
+
+
+  const baseUrl = `http://localhost:3005/api/questionBanks/${qid}`;
+
+  //useEffect for fetching question bank
+  useEffect(()=>{
+    fetchQuestionBanks(baseUrl, setLoading, setQuestionBank)
+   
+  },[])
+
+  //useEffect to updata the states
+  useEffect(()=>{
+    console.log(questionBank)
+    console.log('the fetched question bank above.....')
+    console.log(`for ID ${qid}`)
+    if(questionBank){
+        setExamName(questionBank.examname);
+        setExamLevel(questionBank.examlevel);
+        setTime(questionBank.time)
+        setChaptersMode(questionBank.chaptersMode)
+        setGeneralInstruction(questionBank.generalInstruction || {
+            heading:"",
+            paragraphs:['','','']
+        })
+        setChapters(questionBank.chapters)
+    }
+
+  },[questionBank])
+
+
+
+
+
+  const validateTimeInput = () => {
+    if (time < 0) {
+      toast.error("Time cannot be negative");
+      return false;
+    }
+    return true;
+  };
+
+  const validateForm = () => {
+    if (!examname || !examlevel || !time || !chaptersMode) {
+      toast.error("Please fill all required fields.");
+      return false;
+    }
+    if (!generalInstruction.paragraphs[0]) {
+      toast.error("Please fill the first general instruction paragraph.");
+      return false;
+    }
+    return true;
+  };
+
+  const handleEditProceed = (e) => {
+    e.preventDefault();
+    if (!validateForm() || !validateTimeInput()) return;
+
+    const examData = { 
+      examname, 
+      examlevel, 
+      time, 
+      chaptersMode, 
+      generalInstruction 
+    };
+    console.log("Data to populate ", examData);
+    setLoading(true);
+    axios.put(baseUrl, examData)
+      .then((response) => {
+        console.log("RESPONSE AFTER EDITING: ",response.data );
+        // console.log("exam name: " + response.data.examname);
+        console.log(response.data._id + " is the edited question bank ID");
+        toast.success("Exam successfully edited.");
+        toast.success("Proceeding to edit categories");
+        setShowEditChapters(true);
+        
+      })
+      .catch((error) => {
+        console.log('Error editing exam...', error);
+        toast.error(error.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  const handleSaveDashboard = (e) => {
+    e.preventDefault();
+    if (!validateForm() || !validateTimeInput()) return;
+
+    const examData = { 
+      examname, 
+      examlevel, 
+      time, 
+      chaptersMode, 
+      generalInstruction 
+    };
+    setLoading(true);
+    axios.put(baseUrl, examData)
+      .then(() => {
+        toast.success("Exam successfully edited.");
+        toast.success("navigating to dashboard");
+        setTimeout(() => {
+          navigate("/dashboard");
+        }, 3000);
+      })
+      .catch((error) => {
+        console.log('Error populating exam...', error);
+        toast.error(error.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  const handleParagraphChange = (index, value) => {
+    const newParagraphs = [...generalInstruction.paragraphs];
+    newParagraphs[index] = value;
+    setGeneralInstruction({ ...generalInstruction, paragraphs: newParagraphs });
+  };
+
+  return (
+    <>
+     
+          {loading ? (
+            <Spinner/>
+          ) : showEditChapters ? (<showEditChapters
+          
+          chapters = {chapters}/>):
+            (
+            <div className="max-w-2xl mx-auto p-6 bg-white shadow-md rounded-md">
+              <h2 className="text-xl font-bold mb-4">Create Exam</h2>
+              <form>
+                <FormGroup>
+                  <FormLabel htmlFor="examname" required>
+                    Exam Name:
+                  </FormLabel>
+                  <Input
+                    type="text"
+                    id="examname"
+                    value={examname}
+                    onChange={(e) => setExamName(e.target.value)}
+                    required
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <FormLabel htmlFor="examlevel" required>
+                    Exam Level:
+                  </FormLabel>
+                  <Input
+                    type="text"
+                    id="examlevel"
+                    value={examlevel}
+                    onChange={(e) => setExamLevel(e.target.value)}
+                    required
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <FormLabel htmlFor="time" required>
+                    Time (in minutes):
+                  </FormLabel>
+                  <Input
+                    type="number"
+                    id="time"
+                    value={time}
+                    onChange={(e) => setTime(e.target.value)}
+                    required
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <FormLabel htmlFor="chaptersMode" required>
+                    Chapters Mode:
+                  </FormLabel>
+                  <Select
+                    id="chaptersMode"
+                    value={chaptersMode}
+                    onChange={(e) => setChaptersMode(e.target.value)}
+                    required
+                  >
+                    <option value="">Select Chapters Mode</option>
+                    <option value="auto">Auto</option>
+                    <option value="none">None</option>
+                  </Select>
+                </FormGroup>
+                <FormGroup>
+                  <FormLabel htmlFor="generalInstructionHeading">
+                    General Instruction Heading:
+                  </FormLabel>
+                  <Input
+                    type="text"
+                    id="generalInstructionHeading"
+                    value={generalInstruction.heading}
+                    onChange={(e) => setGeneralInstruction({ ...generalInstruction, heading: e.target.value })}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <FormLabel htmlFor="generalInstructionParagraph1" required>
+                    General Instruction Paragraph 1:
+                  </FormLabel>
+                  <Input
+                    type="text"
+                    id="generalInstructionParagraph1"
+                    value={generalInstruction.paragraphs[0]}
+                    onChange={(e) => handleParagraphChange(0, e.target.value)}
+                    required
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <FormLabel htmlFor="generalInstructionParagraph2">
+                    General Instruction Paragraph 2:
+                  </FormLabel>
+                  <Input
+                    type="text"
+                    id="generalInstructionParagraph2"
+                    value={generalInstruction.paragraphs[1]}
+                    onChange={(e) => handleParagraphChange(1, e.target.value)}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <FormLabel htmlFor="generalInstructionParagraph3">
+                    General Instruction Paragraph 3:
+                  </FormLabel>
+                  <Input
+                    type="text"
+                    id="generalInstructionParagraph3"
+                    value={generalInstruction.paragraphs[2]}
+                    onChange={(e) => handleParagraphChange(2, e.target.value)}
+                  />
+                </FormGroup>
+                <Button onClick={handleEditProceed} type="button">
+                  Proceed to Edit Questions
+                </Button>
+                <Button onClick={handleSaveDashboard} type="button">
+                  Save and Go Back to Dashboard
+                </Button>
+              </form>
+              <ToastContainer />
+            </div>
+          )}
+   
+     
+    </>
+  );
+};
+
+export default EditExam;
