4a2926832edbecb19f415135cbcf5db7bc4bf94a
yomabd <idreesyomi@gmail.com>
2024-06-07 07:42:41 +0100

    make the header more responsive

--- frontend/src/components/dashboard/Header.jsx
+++ frontend/src/components/dashboard/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Header = ({ title }) => {
   return (
-    <header className="bg-gray-800 text-white py-4 px-6 shadow-md">
+    <header className= "fixed left-0 right-0 z-[2] bg-gray-800 text-white py-4 md:px-40 lg:px-60 shadow-md">
       <h1 className="text-2xl font-semibold">{title}</h1>
     </header>
   );
