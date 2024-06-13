e57420266c7560135849b5a04717f2e901f1dee4
yomabd <idreesyomi@gmail.com>
2024-06-13 22:11:40 +0100

    add logo to the components

--- frontend/src/components/dashboard/Header.jsx
+++ frontend/src/components/dashboard/Header.jsx
@@ -4,7 +4,11 @@ import React from 'react';
 const Header = ({ title }) => {
   return (
     <header className= "fixed left-0 right-0 z-[2] bg-gray-800 text-white py-4 md:px-40 lg:px-60 shadow-md">
+      <div className='flex space-x-4'>
+
+       <h1 className="text-5xl font-logo ml-2 z-50 absolute left-0">Exemp</h1>
       <h1 className="text-2xl font-semibold">{title}</h1>
+      </div>
     </header>
   );
 };
--- frontend/src/components/dashboard/Sidebar.jsx
+++ frontend/src/components/dashboard/Sidebar.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 const Sidebar = ({handleSelectedComponent, selectedComponent}) => {
   return (
     <div className="bg-gray-800 text-white -64 space-y-6 py-7 px-2 md:min-h-screen fixed top-0 left-0 w-1/6 ">
+     
       <nav>
         {/* <Link to="/dashboard" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700">Dashboard</Link> 
         <Link to="/create-exam" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700">Create Exam</Link>
