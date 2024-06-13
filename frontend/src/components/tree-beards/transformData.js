29a343e51dee0520d3873084332cd11d69b5f118
yomabd <idreesyomi@gmail.com>
2024-06-13 22:09:19 +0100

    tranform data to the form react tree-beard can work with

--- frontend/src/components/tree-beards/transformData.js
+++ frontend/src/components/tree-beards/transformData.js
@@ -1,23 +1,62 @@
 export const transformData = (questionBank) => {
   return {
-    name: questionBank.examname,
-    toggled: true, // Default state for expanded
-    children: questionBank.chapters.map((chapter) => ({
-      name: chapter.name,
-      toggled: true,
-      children: chapter.questions.map((question) => ({
-        name: question.question,
-        toggled: true,
-        children: question.options.map((option, index) => ({
-          name: `Option ${index + 1}: ${option}${
-            option === question.correctAnswer ? " (Correct Answer)" : ""
-          }`,
+    name: `Exam: ${questionBank.examname}`,
+    toggled: true,
+    children: [
+      {
+        name: `Level: ${questionBank.examlevel}`,
+      },
+      {
+        name: `Time: ${questionBank.time}`,
+      },
+      {
+        name: "General Instruction",
+        children: [
+          {
+            name: `Heading: ${questionBank.GeneralInstruction.heading}`,
+          },
+          ...questionBank.GeneralInstruction.paragraphs.map(
+            (paragraph, index) => ({
+              name: `Paragraph ${index + 1}: ${paragraph}`,
+            })
+          ),
+        ],
+      },
+      {
+        name: "Chapters",
+        children: questionBank.chapters.map((chapter) => ({
+          name: `Chapter: ${chapter.name}`,
+          toggled: true,
+          children: [
+            {
+              name: `Time: ${chapter.time}`,
+            },
+            {
+              name: "Questions",
+              children: chapter.questions.map((question) => ({
+                name: `Question: ${question.question}`,
+                toggled: true,
+                children: question.options.map((option, index) => ({
+                  name: `Option ${index + 1}: ${option}${
+                    option === question.correctAnswer ? " (Correct Answer)" : ""
+                  }`,
+                })),
+              })),
+            },
+            {
+              name: "Instruction",
+              children: [
+                {
+                  name: `Heading: ${chapter.instruction.heading}`,
+                },
+                ...chapter.instruction.paragraphs.map((paragraph, index) => ({
+                  name: `Paragraph ${index + 1}: ${paragraph}`,
+                })),
+              ],
+            },
+          ],
         })),
-      })),
-    })),
-    examlevel: questionBank.examlevel,
-    time: questionBank.time,
-    chaptersMode: questionBank.chaptersMode,
-    GeneralInstruction: questionBank.GeneralInstruction,
+      },
+    ],
   };
 };
