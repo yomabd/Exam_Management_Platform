9e0333854c263529cd52e4ac97a42a8456e7d1f0
yomabd <idreesyomi@gmail.com>
2024-07-11 13:48:50 +0100

    set up public and private routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,9 +6,11 @@ import CreateChapterPage from './components/dashboard/CreateChapterPage';
 import CreateExamPage from './components/dashboard/CreateExamPage';
 import Dashboard from './components/dashboard/Dashboard';
 import ViewResultsPage from './components/dashboard/ViewResultsPage';
-import ExamPage from './components/exam/ExamPage';
+import ExamPage from './components/candidates/ExamPage';
 import ExamsPage from './components/dashboard/ExamsPage';
-import FetchQuestions from './FetchQuestions';
+import FetchQuestions from './components/candidates/FetchQuestions';
+import OverallExamPage from './components/candidates/OverallExamPage';
+import PrivateRoute from './components/authcomponents/PrivateRoute';
 
 
 
@@ -17,16 +19,24 @@ function App() {
   return (
     // <Signin/>
     <div className="">
+      {/* .......Public route.......... */}
     <Routes>
       <Route path='/'  exact element={<Signin/>} />
       <Route path='/signup' element={<Signup/>} />
+      
+      {/* .......Private route.......... */}
+
+      <Route element={<PrivateRoute/>}>      
       <Route path='/dashboard' element={<Dashboard/>}/>
       <Route path='/create-exam' element={<CreateExamPage/>}/>
       <Route path='/view-results' element={<ViewResultsPage />}/>
       <Route path='/exams' element={<ExamsPage/>}/>
       <Route path='/fetchQuestionBank' element={<FetchQuestions/>}/>
       <Route path='/:qid/create-chapters/' element={<CreateChapterPage />}/>
-      <Route path= "/exam/:id" element={<ExamPage/>}/>
+      <Route path= "/exam" element={<ExamPage/>}/>
+      <Route path= "/start-exam" element={<OverallExamPage />}/>
+      <Route path= "/candidate-exam" element={<FetchQuestions/>}/>
+      </Route>
 
 
     </Routes>
