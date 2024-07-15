66f65c5571c3fa8f595b91803323b0a549b6ebb1
yomabd <idreesyomi@gmail.com>
2024-07-15 15:15:09 +0100

    allow token based authentication

--- frontend/src/components/dashboard/fetchQuestionBankFunction.js
+++ frontend/src/components/dashboard/fetchQuestionBankFunction.js
@@ -1,8 +1,17 @@
+import axios from "axios";
 // define fetch function to fetch question banks
 export const fetchQuestionBanks = async (url, setLoading, setQuestionBanks) => {
   try {
     setLoading(true);
-    const response = await fetch(url);
+    const token = localStorage.getItem("token");
+    if (!token) {
+      toast.error("Authorization required, please login");
+      return;
+    }
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+    const response = await axios.get(url, { headers });
     if (!response.ok) {
       throw new Error(`Http error status: ${response.status}`);
     }
