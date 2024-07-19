e0075946e46543dde499a9ea5778a5446bfc8863
yomabd <idreesyomi@gmail.com>
2024-07-19 17:07:06 +0100

    modify the routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -9,9 +9,9 @@ const {
 const router = express.Router();
 
 //maps router to the controllers
-router.post("admin/login", adminLogin);
-router.post("admin/register", adminRegister);
-router.post("candidate/login", candidateLogin);
-router.post("candidate/register", candidateRegister);
+router.post("/admin/login", adminLogin);
+router.post("/admin/register", adminRegister);
+router.post("/candidate/login", candidateLogin);
+router.post("/candidate/register", candidateRegister);
 
 module.exports = router;
