ce9163bd608a4804cbc1f6d2e353bf8d09a28e06
yomabd <idreesyomi@gmail.com>
2024-07-19 11:39:20 +0100

    add candidate and admin routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,17 @@
 const express = require("express");
-const { userRegister, userLogin } = require("../controller/userController");
+const { adminLogin, adminRegister } = require("../controller/adminController");
+const {
+  candidateLogin,
+  candidateRegister,
+} = require("../controller/candidateController");
 
 //create an express router
 const router = express.Router();
 
 //maps router to the controllers
-router.post("/login", userLogin);
-router.post("/register", userRegister);
+router.post("admin/login", adminLogin);
+router.post("admin/register", adminRegister);
+router.post("candidate/login", candidateLogin);
+router.post("candidate/register", candidateRegister);
 
 module.exports = router;
