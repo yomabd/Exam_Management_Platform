4f86629324a951002a9c2e91784f0f8372c15241
yomabd <idreesyomi@gmail.com>
2024-07-26 20:43:41 +0100

    change component importation due to file name change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,6 @@ import Dashboard from './components/dashboard/Dashboard';
 import ViewResultsPage from './components/dashboard/ViewResultsPage';
 import ExamPage from './components/candidates/ExamPage';
 import ExamsPage from './components/dashboard/ExamsPage';
-import FetchQuestions from './components/candidates/FetchQuestions';
 import OverallExamPage from './components/candidates/OverallExamPage';
 import PrivateRoute from './components/authcomponents/PrivateRoute';
 import CandidateSignin from './components/authcomponents/CandidateSignin';
@@ -14,6 +13,7 @@ import CandidateSignup from './components/authcomponents/CandidateSignup';
 import AdminSignin from './components/authcomponents/AdminSignin';
 import AdminSignup from './components/authcomponents/AdminSignup';
 import CandidateDashboard from './components/candidates/CandidateDashboard';
+import FetchExams from './components/candidates/FetchExams';
 
 
 
@@ -41,7 +41,7 @@ function App() {
       <Route path='/:qid/create-chapters/' element={<CreateChapterPage />}/>
       <Route path= "/exam" element={<ExamPage/>}/>
       <Route path= "/start-exam" element={<OverallExamPage />}/>
-      <Route path= "/candidate-exam" element={<FetchQuestions/>}/>
+      <Route path= "/candidate-exam" element={<FetchExams/>}/>
       </Route>
 
 
