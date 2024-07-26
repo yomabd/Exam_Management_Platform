f76ac138917fd5a9a7c62c87c6f615e09d82431f
yomabd <idreesyomi@gmail.com>
2024-07-26 20:38:08 +0100

    build a controller for getting exams for a candidate

--- backend/controller/candidateController.js
+++ backend/controller/candidateController.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const dotenv = require("dotenv");
 const users = require("../models/userModel");
+const QuestionBank = require("../models/questionBankModel");
 
 //make the environment variables available
 dotenv.config();
@@ -210,5 +211,24 @@ exports.getCandidatesWithoutExam = async (req, res) => {
   } catch (error) {
     console.error("Error fetching candidates without exam:", error.message);
     return res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.getCandidateExams = async (req, res) => {
+  try {
+    const user = req.user;
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized! Please log in." });
+    }
+
+    const exams = await QuestionBank.find({ _id: { $in: user.assignedExams } });
+    if (!exams || exams.length === 0) {
+      return res.status(404).json({ message: "No exams available!" });
+    }
+
+    res.status(200).json(exams);
+  } catch (error) {
+    console.error("Error fetching candidate exams:", error.message);
+    res.status(500).json({ message: "Server error" });
   }
 };
