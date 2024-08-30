c1ab33542ada8cba8e9e9f6b2cee707b1b035872
yomabd <idreesyomi@gmail.com>
2024-08-30 18:03:13 +0100

    implement the controller to get a candidate exam

--- backend/controller/candidateController.js
+++ backend/controller/candidateController.js
@@ -232,3 +232,30 @@ exports.getCandidateExams = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+exports.getCandidateExamById = async (req, res) => {
+  try {
+    const userId = req.user?._id;
+    const { id } = req.params;
+
+    if (!userId) {
+      return res.status(401).json({ message: "Unauthorized! Please log in." });
+    }
+
+    const exam = await QuestionBank.findById(id);
+    if (!exam) {
+      return res.status(404).json({ message: "Question bank not found!" });
+    }
+
+    if (!exam.assignedCandidates.includes(userId)) {
+      return res
+        .status(403)
+        .json({ message: "Forbidden! Unauthorized access." });
+    }
+
+    return res.status(200).json(exam);
+  } catch (error) {
+    console.error("Error fetching an exam by ID:", error.message);
+    return res.status(500).json({ message: "Internal server error!" });
+  }
+};
