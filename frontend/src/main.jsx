19c82da4451ae676f46d6cd4c46f4e6a7cac0509
yomabd <idreesyomi@gmail.com>
2024-09-01 21:08:55 +0100

    ensure the axios configured is initialized early

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx'
 import './index.css'
+import api from './configs/axiosConfig';
 import { BrowserRouter } from 'react-router-dom'
 // import { ToastContainer, toast } from "react-toastify";
 // import "react-toastify/dist/ReactToastify.css";
